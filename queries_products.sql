fc66c215dc53cc12667e603814daef65cc4f3cbe
steventang <>
2022-01-31 20:49:32 -0800

    Part 3 complete

--- queries_products.sql
+++ queries_products.sql
@@ -34,3 +34,14 @@ SELECT *  FROM products WHERE price < 44.00;
 -- Display only the products that have a price in between 22.50 and 99.99. --
 SELECT *  FROM products WHERE price BETWEEN 22.50 AND 99.99;
 
+-- There’s a sale going on: Everything is $20 off! Update the database accordingly. --
+UPDATE products SET price = price - 20; 
+
+-- Because of the sale, everything that costs less than $25 has sold out. Remove all products whose price meets this criteria. --
+DELETE FROM products WHERE price < 25;
+
+-- And now the sale is over. For the remaining products, increase their price by $20. --
+UPDATE products SET price = price + 20;
+
+-- There is a new company policy: everything is returnable. Update the database accordingly. --
+UPDATE products SET can_be_returned = true;
