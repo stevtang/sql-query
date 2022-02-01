06f712392d314d13fe637925fd0978ac7a974eb6
Lidan-Huang <huangld0314@gmail.com>
2022-01-31 17:04:08 -0800

    finished questions 6-10

--- queries_products.sql
+++ queries_products.sql
@@ -17,3 +17,20 @@ SELECT * FROM products;
 
 -- Display all of the names of the products. --
 SELECT name FROM products;
+
+-- Display all of the names and prices of the products.--
+SELECT name, price FROM products;
+
+-- Add a new product — make up whatever you would like! -- 
+INSERT INTO products (name, price, can_be_returned) 
+  VALUES ('nightstand', 85.00, 'false');
+
+-- Display only the products that can_be_returned. --
+SELECT *  FROM products WHERE can_be_returned = true;
+
+-- Display only the products that have a price less than 44.00. --
+SELECT *  FROM products WHERE price < 44.00;
+
+-- Display only the products that have a price in between 22.50 and 99.99. --
+SELECT *  FROM products WHERE price BETWEEN 22.50 AND 99.99;
+
